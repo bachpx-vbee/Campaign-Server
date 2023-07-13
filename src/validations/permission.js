866dc140a3bbb5dc7b2be98bfbca0a7b57ff1970
sucrub <bachspham@gmail.com>
2023-07-13 13:49:38 +0700

    Update permission

--- src/validations/permission.js
+++ src/validations/permission.js
@@ -29,10 +29,29 @@ const getPermissions = {
   }),
 };
 
+const updatePermission = {
+  params: Joi.object({
+    permissionId: Joi.string()
+      .custom((value, helpers) =>
+        validateObjectId("permissionId", value, helpers)
+      )
+      .messages({
+        "any.invalid": "permissionId must be a valid objectId",
+      }),
+  }),
+  body: Joi.object({
+    name: Joi.string().trim().required(),
+    method: Joi.string().trim().required(),
+    url: Joi.string().trim().required(),
+    type: Joi.string().trim().required(),
+  }),
+};
+
 module.exports = {
   createPermissionValidate: validate(createPermission, { keyByField: true }),
   deletePermissionByIdValidate: validate(deletePermissionById, {
     keyByField: true,
   }),
   getPermissionsValidate: validate(getPermissions, { keyByField: true }),
+  updatePermissionValidate: validate(updatePermission, { keyByField: true }),
 };
